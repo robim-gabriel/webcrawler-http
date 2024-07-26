44b0aee992698b31d087698eb3175cc0ec647762
robim-gabriel <fpgr07@gmail.com>
2024-07-26 15:45:33 +0100

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,25 +1,54 @@
+const jsdom = require("jsdom")
+const { JSDOM } = jsdom
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = []
+  const dom = new JSDOM(htmlBody)
+  const anchors = dom.window.document.querySelectorAll('a')
+
+  for (const anchor of anchors) {
+    if (anchor.hasAttribute('href')) {
+      let href = anchor.getAttribute('href')
+
+      if (href.length === 0) {
+        continue
+      }
+
+      try {
+        href = new URL(href, baseURL).href
+        urls.push(href)
+      } catch (err) {
+        console.log(`Error: ${err.message}`)
+      }
+    }
+  }
+
+  return urls
+}
+
 function normalizeURL(urlString) {
-	
-	if (urlString.length === 0) {
-		throw new TypeError('Empty urlString')
-	}
 
-	let urlObj
-	try {
-		urlObj = new URL(urlString)
-	} catch(err) {
-		throw new TypeError('Invalid urlString')	
-	}
+  if (urlString.length === 0) {
+    throw new TypeError('Empty urlString')
+  }
 
-	const normalized = `${urlObj.hostname}${urlObj.pathname}`
-	
-	if (normalized.slice(-1) === '/') {
-		return normalized.slice(0,-1)
-	}
+  let urlObj
+  try {
+    urlObj = new URL(urlString)
+  } catch (err) {
+    throw new TypeError('Invalid urlString')
+  }
 
-	return normalized
+  const normalized = `${urlObj.hostname}${urlObj.pathname}`
+
+  if (normalized.slice(-1) === '/') {
+    return normalized.slice(0, -1)
+  }
+
+  return normalized
 }
 
 module.exports = {
-	normalizeURL
+  normalizeURL,
+  getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,42 +1,62 @@
-const { normalizeURL } = require('./crawl.js')
-const {test, expect } = require('@jest/globals')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
+const { test, expect } = require('@jest/globals')
 
-test('normalizeURL strip protocol', () => { 
-	const input = 'https://muchas-gracias-afficion/siuuuuu'
-	const actual = normalizeURL(input)
-	const expected = 'muchas-gracias-afficion/siuuuuu'
-	expect(actual).toEqual(expected)
+test('normalizeURL protocol', () => {
+  const input = 'https://blog.boot.dev/path'
+  const actual = normalizeURL(input)
+  const expected = 'blog.boot.dev/path'
+  expect(actual).toEqual(expected)
 })
 
-test('normalizeURL strip protocol with full url example', () => { 
-	const input = 'https://ruigay:euseihehehsiuu123@fantasyquest.com:8080/maps?sort=rank#id'
-	const actual = normalizeURL(input)
-	const expected = 'fantasyquest.com/maps'
-	expect(actual).toEqual(expected)
+test('normalizeURL slash', () => {
+  const input = 'https://blog.boot.dev/path/'
+  const actual = normalizeURL(input)
+  const expected = 'blog.boot.dev/path'
+  expect(actual).toEqual(expected)
 })
 
-test('normalizeURL strip protocol throws TypeError with empty input', () => {
-	const input = ''
-	expect(() => normalizeURL(input)).toThrow(TypeError)
-	expect(() => normalizeURL(input)).toThrow('Empty urlString')
+test('normalizeURL capitals', () => {
+  const input = 'https://BLOG.boot.dev/path'
+  const actual = normalizeURL(input)
+  const expected = 'blog.boot.dev/path'
+  expect(actual).toEqual(expected)
 })
 
-test('normalizeURL strip protocol correctly trims trailing slash', () => {
-	const input = 'https://boot.dev/leaderboard/'
-	const actual = normalizeURL(input)
-	const expected = 'boot.dev/leaderboard'
-	expect(actual).toEqual(expected)
+test('normalizeURL http', () => {
+  const input = 'http://BLOG.boot.dev/path'
+  const actual = normalizeURL(input)
+  const expected = 'blog.boot.dev/path'
+  expect(actual).toEqual(expected)
 })
 
-test('normalizeURL strip protocol throws TypeError with an invalid urlString', () => {
-	const input = 'this-is-not-a-valid-url-string'
-	expect(() => normalizeURL(input)).toThrow(TypeError)
-	expect(() => normalizeURL(input)).toThrow('Invalid urlString')
+test('getURLsFromHTML absolute', () => {
+  const inputURL = 'https://blog.boot.dev'
+  const inputBody = '<html><body><a href="https://blog.boot.dev"><span>Boot.dev></span></a></body></html>'
+  const actual = getURLsFromHTML(inputBody, inputURL)
+  const expected = ['https://blog.boot.dev/']
+  expect(actual).toEqual(expected)
 })
 
-test('normalizeURL strip protocol is capital insensitive', () => {
-	const input = 'http://bLoG.WEBSITE.DEV/testpath'
-	const actual = normalizeURL(input)
-	const expected = 'blog.website.dev/testpath'
-	expect(actual).toEqual(expected)
+test('getURLsFromHTML relative', () => {
+  const inputURL = 'https://blog.boot.dev'
+  const inputBody = '<html><body><a href="/path/one"><span>Boot.dev></span></a></body></html>'
+  const actual = getURLsFromHTML(inputBody, inputURL)
+  const expected = ['https://blog.boot.dev/path/one']
+  expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML both', () => {
+  const inputURL = 'https://blog.boot.dev'
+  const inputBody = '<html><body><a href="/path/one"><span>Boot.dev></span></a><a href="https://other.com/path/one"><span>Boot.dev></span></a></body></html>'
+  const actual = getURLsFromHTML(inputBody, inputURL)
+  const expected = ['https://blog.boot.dev/path/one', 'https://other.com/path/one']
+  expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML empty href', () => {
+  const inputURL = 'https://blog.boot.dev'
+  const inputBody = '<html><body><a href=""><span>Boot.dev></span></a><a href="https://other.com/path/one"><span>Boot.dev></span></a></body></html>'
+  const actual = getURLsFromHTML(inputBody, inputURL)
+  const expected = ['https://other.com/path/one']
+  expect(actual).toEqual(expected)
 })
